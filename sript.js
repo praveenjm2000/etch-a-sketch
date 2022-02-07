7ad60d60ab6d4af9d9330325a30d18600ad97b4e
Praveen <praveenjm2000@gmail.com>
2022-02-08 01:06:02 +0530

    Basic ui and functionalities done

--- sript.js
+++ sript.js
@@ -4,15 +4,48 @@ const btn42 = document.querySelector('.btn64');
 
 
 const container=document.querySelector('.board');
-let x=19;
-let w=(480/x);
-console.log(w);
-for(let i=0;i<x*x;i++){
-    const box=document.createElement('div');
-    box.setAttribute('style',`width: ${w}px; height: ${w}px;`);
-    box.addEventListener('mouseenter',() => {
-        box.classList.add('boxClicked');
-    });
-    container.appendChild(box);
+let gridSize=32;
+generate();
+
+function generate(){
+    let x=gridSize;
+    let w=(480/x);
+    for(let i=0;i<x*x;i++){
+        const box=document.createElement('div');
+        box.classList.add('box');
+        box.setAttribute('style',`width: ${w}px; height: ${w}px;`);
+        box.addEventListener('mouseenter',() => {
+            box.classList.add('boxClicked');
+        });
+        container.appendChild(box);
+    }
 }
 
+function clear(){
+    let x=gridSize;
+    for(let i=0;i<x*x;i++){
+        box=document.querySelector('.box');
+        container.removeChild(box);
+    }
+}
+
+let submitBtn=document.getElementById('submit');
+submitBtn.addEventListener('click',()=> {
+    clear();
+    gridSize=document.getElementById('grid').value;
+    generate();
+});
+
+let clearBtn=document.getElementById('clear');
+clearBtn.addEventListener('click',()=> {
+    clear();
+    generate();
+});
+
+let black=document.getElementById('black');
+let rgb=document.getElementById('rgb');
+black.addEventListener('click',()=> {
+    boxcolor='black'
+    clear()
+    generate();
+})
